8da6600228cee0a82d4aaf2badca1d113b6d647c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-06-05 17:20:49 +0300

    umqtt.publish: publish(): Make retain arg go before qos.
    Likely, retain will be used more often than qos.

--- umqtt/umqtt/simple.py
+++ umqtt/umqtt/simple.py
@@ -55,7 +55,7 @@ class MQTTClient:
         self.sock.write(b"\xc0\0")
         self.sock.close()
 
-    def publish(self, topic, msg, qos=0, retain=False):
+    def publish(self, topic, msg, retain=False, qos=0):
         assert qos == 0
         pkt = bytearray(b"\x30\0\0")
         pkt[0] |= qos << 1 | retain
